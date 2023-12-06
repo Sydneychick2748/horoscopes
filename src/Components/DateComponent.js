5ff6acbefed394028c0f7981545dce1ba9139852
ana <ana@anniecannons.com>
2023-12-06 12:03:41 -0800

    added the use effe ct

--- src/Components/DateComponent.js
+++ src/Components/DateComponent.js
@@ -1,65 +1,57 @@
-
-
-
+import { useState,useEffect } from "react";
 
 function DateComponent(props) {
-    console.log(props.data, "props for date")
- 
-// Get the current date
-const currentDate = new Date();
+  console.log(props.data, "props for date");
+  const [newObjects, setNewObjects] = useState([]);
 
-// Get day, month, and year
-const day = currentDate.getDate();
-const month = currentDate.getMonth() + 1; // Months are zero-indexed, so add 1
-const year = currentDate.getFullYear();
+  // Get the current date
+  const currentDate = new Date().toLocaleDateString();
+  console.log(currentDate, "currentDate");
 
+  useEffect(() => {
+    // Extract all date ranges from the zodiac signs and map over the dateRange
+    const zodiacSignDates = props.data.map((sign) => {
+      const dateRangeArray = sign.dateRange.split(" - ");
+      const startDate = dateRangeArray[0];
+      const endDate = dateRangeArray[1];
+      // push in the sign and then update the object so that is contains the date object
+      const updatedSign = {
+        ...sign,
+        startDate: new Date(startDate).toLocaleDateString(),
+        endDate: new Date(endDate).toLocaleDateString(),
+      };
 
-// Format the date as "day/month/year"
-const formattedDate = `${day}/${month}/${year}`;
-console.log(formattedDate, "formattteddate")
+      return updatedSign;
+    });
 
-//Extract all date ranges from the zodiac signs and map over the dateRange
-const allDateRanges = props.data.map((sign) => sign.dateRange);
-console.log(allDateRanges, "alldates")
-
-//  extracting the start and end dates for a zodiac sign from the dateRange property and converting them into JavaScript Date objects.
-
-
-const zodiacSignDates = allDateRanges.map((range) => {
-    const dateRangeArray = range.split(" - ");
-const startDate = dateRangeArray[0];
-const endDate = dateRangeArray[1];
-
-return {
-    startDate: new Date(startDate),
-    endDate: new Date(endDate),
-  };
+    setNewObjects(zodiacSignDates); // Update state with the new array
+  }, [props.data]);
 
   
 
-
-})
-
-console.log(zodiacSignDates, "zodiac sign dates");
-
-// now i have my 2 dates now i want to be able to find the sign that matches with the 2 dates that i have 
+  console.log(newObjects, "zodiac sign dates");
 
 
+  // now i have my 2 dates now i want to be able to find the sign that matches with the 2 dates that i have
+  // Find the zodiac sign that matches the current date
+  const currentZodiacSign = newObjects.find((signs) => {
+    console.log(signs , "signs")
+    console.log("Comparing:", currentDate, signs.startDate, signs.endDate);
+    return currentDate >= signs.startDate && currentDate <= signs.endDate;
+  });
 
+  const zodiacSignName = currentZodiacSign ? currentZodiacSign.sign : "Unknown";
+  console.log(zodiacSignName, "zodiacNames");
+  console.log(currentZodiacSign, "currentzs")
+    
+  return (
+    <div className="date-container">
+      {currentDate}
 
-
-    return (
-     
- <div className="date-container">
-    {formattedDate}
-    <p>The current Zodiac sign is : </p>
-   
+      <p>Your current sign is: {zodiacSignName}</p>
     </div>
+  );
   
-      
-      
-    );
-  }
-  
-  export default DateComponent;
-  
+    }
+
+    export default DateComponent;
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -4,24 +4,25 @@
 // 3. The user should be able to toggle on and off
 //4. if the button is clicked then it should show hide on the button instead of start and if the hide button is showing a text will also appear saying "appear"
 
-import { useState } from "react";
+import React, { useState } from "react";
 import HoroscopePicker from "../Components/HoroscopePicker";
 import DateComponent from "../Components/DateComponent";
 
 function Home({ dataProps }) {
   console.log(dataProps, "dataProps");
   const data = dataProps.horoscopes.astroSigns;
-  
+
   const [showText, setShowText] = useState(false);
   const [noBtnText, setNoBtnText] = useState(false);
   const [yesBtnData, setYesBtnData] = useState([]);
-  const [seeDateText,setSeeDatetext] = useState(false)
-  
+  const [seeDateText, setSeeDateText] = useState(false);
+  const [dateComponentVisible, setDateComponentVisible] = useState(false);
 
   function startBtnClick() {
     console.log("clicked");
     setShowText(!showText);
   }
+
   function yesBtnClick() {
     console.log("clicked");
     const signNames = data.map((item) => item.sign);
@@ -34,12 +35,11 @@ function Home({ dataProps }) {
     setNoBtnText(true);
   }
 
-  function seeDateClick(){
-    console.log("clikced")
-    setSeeDatetext(!seeDateText)
+  function seeDateClick() {
+    console.log("clicked");
+    setSeeDateText(!seeDateText);
+    setDateComponentVisible(!dateComponentVisible); // Toggle visibility of DateComponent
   }
-
-  
 
   return (
     <div className="home-container">
@@ -50,25 +50,26 @@ function Home({ dataProps }) {
           <h2>Learn About Your Horoscope !</h2>
           <p>Would you like to choose an astrological sign</p>
           <div>
-          <button className="seeDateBtn" onClick={seeDateClick}>
-{seeDateText? "Click here to hide todays date": "click here to see todays Date"}
-
-          </button>
-          <DateComponent data={data}/>
+            <button className="seeDateBtn" onClick={seeDateClick}>
+              {seeDateText
+                ? "Click here to hide today's date"
+                : "Click here to see today's Date"}
+            </button>
+            {dateComponentVisible ? <DateComponent data={data} /> : null}
           </div>
-       
-          {/* if you click on the button i want to display the data for the boxes of signs */}
+
+          {/* if you click on the button I want to display the data for the boxes of signs */}
           <button className="yesBtn" onClick={yesBtnClick}>
-            Yes Please!
+            Yes, Please!
           </button>
-          {yesBtnData.length > 0 && <HoroscopePicker data={data} yesBtnDataProps={yesBtnData} />}
+          {yesBtnData.length > 0 && (
+            <HoroscopePicker data={data} yesBtnDataProps={yesBtnData} />
+          )}
 
           {/* this has to show some text saying sorry to see you go when clicked */}
           <button className="noBtn" onClick={noBtnClick}>
-            
-            No Thankyou!
+            No, Thank you!
           </button>
-
 
           <p>{noBtnText ? "Sorry to see you go" : null}</p>
         </div>
